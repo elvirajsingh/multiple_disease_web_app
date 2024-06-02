689e581f7ff3cf57cf594b5ddf29736939131f41
Viraj Singh <120745872+elvirajsingh@users.noreply.github.com>
2024-06-03 02:54:30 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('"C:\Users\Viraj\Downloads\diabetes_model (1).sav"', 'rb'))
+diabetes_model = pickle.load(open('C:\Users\Viraj\Downloads\diabetes_model (1).sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('C:\Users\Viraj\Downloads\heart_disease_model (1).sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('C:\Users\Viraj\Downloads\parkinsons_model.sav', 'rb'))
 
 
 
